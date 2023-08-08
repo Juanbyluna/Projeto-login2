8982fb45c59402cacc72b53e7e6cc24f6cff8ea9
Juan/Luna Fernandes <juanfernandesdegoes@gmail.com>
2023-08-08 10:22:18 -0300

    finalização do projeto

--- controller.js
+++ controller.js
@@ -3,27 +3,33 @@ function  Entrar(){
 }
 //cria lista de usuario dentro de um array
      var dadosLista = [];
+     var tipoTask = [];
      function salvarUser(){
         let nomeUser = document.getElementById("nomeUser").value;
+        let task = document.getElementById('task').value;
+        
         if(nomeUser){
             dadosLista.push(nomeUser);
+            tipoTask.push(task);
             criaLista();
             document.getElementById("nomeUser").value = '';
         }
      }
     //  FUNÇAO PARA CRIACAO DE LISTA NA TABELA
     function criaLista(){
-        let tabela = document.getElementById('tabela').innerHTML = " <tr><th>itens</th><th></th></tr>";
+        let tabela = document.getElementById('tabela').innerHTML = " <tr><th>tarefa</th><th>tipo da tarefa</th><th>Ações</th></tr>";
         // LAÇO DE REPETICAO PARA COLOCAR NOMES NA TABELA
         for(let i=0;i <=(dadosLista.length-1);i++){
-            tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+            tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + tipoTask[i]+ "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
             document.getElementById("tabela").innerHTML = tabela;
         }
     }
     // funcao para editar nome
     function editar(i){
         document.getElementById("nomeUser").value = dadosLista[(i - 1)];
-        dadosLista.splice(dadosLista [(i - 1)], 1);
+        dadosLista.splice(dadosLista [(i - 1)], 1); 
+        document.getElementById("task").value = tipoTask[(i - 1)];
+        tipoTask.splice(tipoTask [(i - 1)], 1);
     }
     // funcao para excluir nome
     function excluir(i){
